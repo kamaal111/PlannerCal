70703a440d4a9a135b9d48eda526cfb15222aec4
kamaal111 <kamaal@tranzer.com>
2021-03-31 18:35:11 +0200

    Constantly getting screen size

--- Shared/PlannerCalApp.swift
+++ Shared/PlannerCalApp.swift
@@ -9,12 +9,6 @@ import SwiftUI
 
 @main
 struct PlannerCalApp: App {
-    #if canImport(AppKit)
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    #elseif canImport(UIKit)
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    #endif
-
     @StateObject private var navigator = Navigator()
     @StateObject private var deviceModel = DeviceModel()
 
@@ -22,27 +16,15 @@ struct PlannerCalApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container!.viewContext)
-                .environmentObject(navigator)
-                .environmentObject(deviceModel)
+            GeometryReader { geometry -> ContentView in
+                if deviceModel.screenSize != geometry.size {
+                    deviceModel.screenSize = geometry.size
+                }
+                return ContentView()
+            }
+            .environment(\.managedObjectContext, persistenceController.container!.viewContext)
+            .environmentObject(navigator)
+            .environmentObject(deviceModel)
         }
     }
 }
-
-#if canImport(AppKit)
-final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        if let app = notification.object as? NSApplication {
-            print(app)
-        }
-    }
-}
-#elseif canImport(UIKit)
-final class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print(application)
-        return true
-    }
-}
-#endif
