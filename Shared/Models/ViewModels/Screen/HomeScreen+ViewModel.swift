f6b4f1d7ba7064375eec0d01c994fbc0e130aa8a
kamaal111 <kamaal@tranzer.com>
2021-04-02 09:16:06 +0200

    Getting initial plan days

--- Shared/Models/ViewModels/Screen/HomeScreen+ViewModel.swift
+++ Shared/Models/ViewModels/Screen/HomeScreen+ViewModel.swift
@@ -5,12 +5,20 @@
 //  Created by Kamaal Farah on 02/04/2021.
 //
 
-import Combine
-import CoreGraphics
+import Foundation
+import ShrimpExtensions
 
 extension HomeScreen {
+
     final class ViewModel: ObservableObject {
+
         @Published private(set) var viewWidth: CGFloat = 100
+        /// - TODO: Should be in a seperate enviroment view model
+        @Published private(set) var currentDays: [Date] = [] {
+            didSet {
+                print(currentDays)
+            }
+        }
 
         let days = [
             "Mon",
@@ -18,9 +26,18 @@ extension HomeScreen {
             "Wed"
         ]
 
+        init() {
+            let today = Date()
+            self.currentDays = (0..<5).compactMap {
+                Calendar.current.date(byAdding: .day, value: $0 - 1, to: today)
+            }
+        }
+
         func setViewWidth(with width: CGFloat) {
             guard width != viewWidth else { return }
             viewWidth = width
         }
+
     }
+
 }
